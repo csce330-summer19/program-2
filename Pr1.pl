e31f0e385b750e0fe0669fe73cc7254827d0ec91
oreillyj <oreillyj.work@gmail.com>
2018-09-07 14:44:12 -0400

    starter code

---
+++ Pr1.pl
@@ -0,0 +1,33 @@
+% you need to replace, e.g., "D='fix this'" with a valid query.
+
+%Did Leonardo DiCaprio act in Babel?
+q1 :- true.
+
+%Who directed District 9?
+q2(D) :- D='fix this'.
+
+%Did anyone act in Both Click and The Aviator
+q3(A) :- A='fix this'.
+
+%Was there a move released in 2010 that did not star Jennifer Aniston?
+q4(M):-  M='fix this'.
+
+%Who directed movies released in 2009? (don't worry about ; part)
+q5(D):-  D='fix this'.
+
+%Has anyone directed more than one movie?
+q6(D) :- D='fix this'.
+
+%Does any movie have more than one director?
+q7(M) :- M='fix this'.
+
+%Has anyone acted in more than one movie released in 2008?
+q8(A) :- A='fix this'.
+
+%Has anyone acted in more than two movies in the same year?
+q9(A) :- A='fix this'.
+
+%Who has worked with the same director in different years?
+q10(A) :- A='fix this'.
+
+
